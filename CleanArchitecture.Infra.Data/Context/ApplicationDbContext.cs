f7cda2159bba000d9a4336f18a3f5c58f86713fd
Danilo Silva <danilo.silva@msn.com>
2024-07-06 16:49:28 -0300

    Criando a Estrutura do Projeto e Contexto

---
+++ CleanArchitecture.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infra.Data.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+    public DbSet<Produto> Produtos { get; set; } = default!;
+    public DbSet<Categoria> Categorias { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
