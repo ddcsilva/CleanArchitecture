8093be1d10a2f35d08881a54c27bb1039ac6faab
Danilo Silva <danilo.silva@msn.com>
2024-07-06 17:02:50 -0300

    Configurando as entidades usando a Fluent API

---
+++ CleanArchitecture.Infra.Data/Configuration/ProdutoConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infra.Data.Configuration;
+
+public class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+{
+    public void Configure(EntityTypeBuilder<Produto> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Nome)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(p => p.Descricao)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(p => p.Preco)
+            .HasPrecision(10, 2);
+
+        builder.HasOne(p => p.Categoria)
+            .WithMany(c => c.Produtos)
+            .HasForeignKey(p => p.CategoriaId);
+    }
+}
