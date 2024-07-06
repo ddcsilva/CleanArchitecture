016c013fd81704735dfc8a3c600a2cfab4252d49
Danilo Silva <danilo.silva@msn.com>
2024-07-06 17:11:47 -0300

    Implementando o Repositório de Categoria

--- CleanArchitecture.Domain/Interfaces/ICategoriaRepository.cs
+++ CleanArchitecture.Domain/Interfaces/ICategoriaRepository.cs
@@ -6,7 +6,7 @@ public interface ICategoriaRepository
 {
     Task<IEnumerable<Categoria>> ObterTodosAsync();
     Task<Categoria> ObterPorIdAsync(int? id);
-    Task AdicionarAsync(Categoria categoria);
-    Task AtualizarAsync(Categoria categoria);
-    Task RemoverAsync(Categoria categoria);
+    Task<Categoria> AdicionarAsync(Categoria categoria);
+    Task<Categoria> AtualizarAsync(Categoria categoria);
+    Task<Categoria> RemoverAsync(Categoria categoria);
 }
---
+++ CleanArchitecture.Infra.Data/Repositories/CategoriaRepository.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infra.Data.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoriaRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Categoria>> ObterTodosAsync()
+    {
+        return await _context.Categorias.ToListAsync();
+    }
+
+    public async Task<Categoria> ObterPorIdAsync(int? id)
+    {
+        return await _context.Categorias.FindAsync(id);
+    }
+
+    public async Task<Categoria> AdicionarAsync(Categoria categoria)
+    {
+        _context.Categorias.Add(categoria);
+        await _context.SaveChangesAsync();
+        return categoria;
+    }
+
+    public async Task<Categoria> AtualizarAsync(Categoria categoria)
+    {
+        _context.Categorias.Update(categoria);
+        await _context.SaveChangesAsync();
+        return categoria;
+    }
+
+    public async Task<Categoria> RemoverAsync(Categoria categoria)
+    {
+        _context.Categorias.Remove(categoria);
+        await _context.SaveChangesAsync();
+        return categoria;
+    }
+}
