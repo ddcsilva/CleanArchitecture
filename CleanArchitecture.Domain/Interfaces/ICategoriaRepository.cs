5ecf61cf8ede4dd8423a69cb37bcbc93328ad04d
Danilo Silva <danilo.silva@msn.com>
2024-07-06 16:04:40 -0300

    Criando Interfaces

---
+++ CleanArchitecture.Domain/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Interfaces;
+
+public interface ICategoriaRepository
+{
+    Task<IEnumerable<Categoria>> ObterTodosAsync();
+    Task<Categoria> ObterPorIdAsync(int? id);
+    Task AdicionarAsync(Categoria categoria);
+    Task AtualizarAsync(Categoria categoria);
+    Task RemoverAsync(Categoria categoria);
+}
---
+++ CleanArchitecture.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Interfaces;
+
+public interface IProdutoRepository
+{
+    Task<IEnumerable<Produto>> ObterTodosAsync();
+    Task<Produto> ObterPorIdAsync(int? id);
+    Task AdicionarAsync(Produto produto);
+    Task AtualizarAsync(Produto produto);
+    Task RemoverAsync(Produto produto);
+}
